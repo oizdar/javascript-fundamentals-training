52fc116b9ab469788f55bc8f8a5e8743c3d9dd31
Radosław Malinowski <malinowski.rad@gmail.com>
2022-09-05 15:39:40 +0200

    made RepLogApp instantiable using prototype object and extract method in both Helper & RepLogApp objects

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,26 +1,26 @@
 'use strict';
 
 (function(window, $) {
-    window.RepLogApp = {
-        initialize: function ($wrapper) {
-            this.$wrapper = $wrapper;
-            this.helper = new Helper($wrapper)
+    window.RepLogApp = function ($wrapper) {
+        this.$wrapper = $wrapper;
+        this.helper = new Helper($wrapper)
 
-            this.$wrapper.find('.js-delete-rep-log').on(
-                'click',
-                this.handleRepLogDelete.bind(this)
-            );
+        this.$wrapper.find('.js-delete-rep-log').on(
+            'click',
+            this.handleRepLogDelete.bind(this)
+        );
 
-            this.$wrapper.find('tbody tr').on(
-                'click',
-                this.handleRowClick.bind(this)
-            )
+        this.$wrapper.find('tbody tr').on(
+            'click',
+            this.handleRowClick.bind(this)
+        )
 
-            console.log(this.helper, Object.keys(this.helper));
-            console.log(Helper, Object.keys(Helper));
-            console.log(this.helper.calculateTotalWeight());
-        },
+        console.log(this.helper, Object.keys(this.helper));
+        console.log(Helper, Object.keys(Helper));
+        console.log(this.helper.calculateTotalWeight());
+    };
 
+    $.extend(window.RepLogApp.prototype, {
         handleRepLogDelete: function (e) {
             e.preventDefault()
             let $link = $(e.currentTarget);
@@ -53,7 +53,7 @@
         handleRowClick: function () {
             console.log('row clicked');
         },
-    };
+    })
 
     /**
      * A "private" object
@@ -62,14 +62,17 @@
         this.$wrapper = $wrapper;
     };
 
-    Helper.prototype.calculateTotalWeight = function () {
-        let totalWeight = 0;
-        this.$wrapper.find('tbody tr').each(function () {
-            totalWeight += $(this).data('weight');
-        });
+    $.extend(Helper.prototype, {
+        calculateTotalWeight: function () {
+            let totalWeight = 0;
+            this.$wrapper.find('tbody tr').each(function () {
+                totalWeight += $(this).data('weight');
+            });
 
-        return totalWeight;
-    }
+            return totalWeight;
+        }
+    });
+
 })(window, jQuery);
 
 
