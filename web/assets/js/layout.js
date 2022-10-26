af522c9262427b6fafd0cf54148475d423bda28a
Radosław Malinowski <malinowski.rad@gmail.com>
2022-10-26 17:34:19 +0200

    load font awesome with webpack

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -3,6 +3,7 @@ const $ = require('jquery');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 require('../css/main.css');
+require('font-awesome/css/font-awesome.css');
 
 //include this on every page to make sure Promise exists
 require('babel-polyfill');
